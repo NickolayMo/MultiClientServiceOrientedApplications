b4afb028c1efd73e7ab9600f06965648c96887c8
Mokrenko Nickolay <kolya456@gmail.com>
2016-05-28 23:29:28 +0300

    1209 adding templates

---
+++ CarRental.Web/Binding/ViewModels/AccountRegisterViewModel.js
@@ -0,0 +1,13 @@
+﻿var accountRegisterModule = angular.module('accountRegister',['common']);
+
+accountRegisterModule.controller('AccountRegisterViewModel', function($scope, $http, $location, $window, viewModelHelper){
+    $scope.viewModelHelper = viewModelHelper;
+    $scope.accountModelStep1 = new CarRental.AccountRegisterModelStep1();
+    $scope.accountModelStep2 = new CarRental.AccountRegisterModelStep2();
+    $scope.accountModelStep3 = new CarRental.AccountRegisterModelStep3();
+    
+    $scope.previous = function(){
+        $window.history.back();
+    }
+
+});
